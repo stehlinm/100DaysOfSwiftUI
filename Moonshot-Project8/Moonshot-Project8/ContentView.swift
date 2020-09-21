c8178339b3a3b7f7c450a198fb43b9b1050bfc45
Marc Stehlin <stehlin.m@gmail.com>
2020-09-21 20:56:47 +0200

    Added Moonshot-Project8

---
+++ Moonshot-Project8/Moonshot-Project8/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot-Project8
+//
+//  Created by marc on 21.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moonshot-Project8/Moonshot-Project8/Moonshot_Project8App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot_Project8App.swift
+//  Moonshot-Project8
+//
+//  Created by marc on 21.09.20.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot_Project8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
