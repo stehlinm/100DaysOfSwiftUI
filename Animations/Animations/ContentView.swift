fd28ba98f18bb6d09a7eeec9bf45530e2997342b
Marc Stehlin <stehlin.m@gmail.com>
2020-09-21 07:56:17 +0200

    Initial import

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by marc on 17.09.20.
+//  Copyright © 2020 marc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isShowingRed = false
+    
+    var body: some View {
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    self.isShowingRed.toggle()
+                }
+            }
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(Color.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by marc on 18.09.20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
