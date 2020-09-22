c7e48b0ab9a8e269be9b4c1d843aec95f0b0645e
Marc Stehlin <stehlin.m@gmail.com>
2020-09-22 21:30:57 +0200

    Added formatted date decoding strategy; added view of the missions

--- Moonshot-Project8/Moonshot-Project8/ContentView.swift
+++ Moonshot-Project8/Moonshot-Project8/ContentView.swift
@@ -5,9 +5,22 @@ struct ContentView: View {
         let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
         let missions: [Mission] = Bundle.main.decode("missions.json")
         
-        VStack {
-            Text("\(astronauts.count)")
-            Text("\(missions.count)")
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail view for \(mission.displayName)")) {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 44, height: 44)
+
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.formattedLaunchDate)
+                    }
+                }
+            }
+            .navigationBarTitle("Moonshot")
         }
     }
 }
