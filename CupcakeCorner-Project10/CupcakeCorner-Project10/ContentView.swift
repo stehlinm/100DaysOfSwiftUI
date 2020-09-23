b02dcbdf36caedc22eca4b0e824f3c1ae41c53f7
Marc Stehlin <stehlin.m@gmail.com>
2020-09-23 11:28:33 +0200

    Added CupcakeCorner-Project10

---
+++ CupcakeCorner-Project10/CupcakeCorner-Project10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner-Project10
+//
+//  Created by marc on 23.09.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner-Project10/CupcakeCorner-Project10/CupcakeCorner_Project10App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_Project10App.swift
+//  CupcakeCorner-Project10
+//
+//  Created by marc on 23.09.20.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_Project10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
