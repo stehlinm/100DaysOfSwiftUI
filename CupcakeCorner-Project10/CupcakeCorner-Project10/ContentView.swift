2f81edd772cda31d5797578cc72c6a449db89ff9
Marc Stehlin <stehlin.m@gmail.com>
2020-09-25 21:08:52 +0200

    Encodable @Published properties

--- CupcakeCorner-Project10/CupcakeCorner-Project10/ContentView.swift
+++ CupcakeCorner-Project10/CupcakeCorner-Project10/ContentView.swift
@@ -1,40 +1,40 @@
-//
-//  ContentView.swift
-//  CupcakeCorner-Project10
-//
-//  Created by marc on 23.09.20.
-//
-
 import SwiftUI
 
-class User: ObservableObject, Codable {
-
-    enum CodingKeys: CodingKey {
+class User: ObservableObject, Codable
+{
+    enum CodingKeys: CodingKey
+    {
         case name
     }
 
     @Published var name = "Paul Hudson"
 
-    required init(from decoder: Decoder) throws {
+    required init(from decoder: Decoder) throws
+    {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         name = try container.decode(String.self, forKey: .name)
     }
 
-    func encode(to encoder: Encoder) throws {
+    func encode(to encoder: Encoder) throws
+    {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(name, forKey: .name)
     }
 }
 
-struct ContentView: View {
-    var body: some View {
+struct ContentView: View
+{
+    var body: some View
+    {
         Text("Hello, world!")
             .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
+struct ContentView_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
         ContentView()
     }
 }
