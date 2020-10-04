2456af702a45347b094ee38a3d624c3c3112db15
Marc Stehlin <stehlin.m@gmail.com>
2020-10-04 13:46:31 +0200

    Added Emoji rating view and Detail view (with images)

--- Bookworm-Project11/Bookworm-Project11/ContentView.swift
+++ Bookworm-Project11/Bookworm-Project11/ContentView.swift
@@ -3,21 +3,26 @@ import SwiftUI
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
     @FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
-
+    
     @State private var showingAddScreen = false
     
     var body: some View {
         NavigationView {
-            Text("Count: \(books.count)")
-                .navigationBarTitle("Bookworm")
-                .navigationBarItems(trailing: Button(action: {
-                    self.showingAddScreen.toggle()
-                }) {
-                    Image(systemName: "plus")
-                })
-                .sheet(isPresented: $showingAddScreen) {
-                    AddBookView().environment(\.managedObjectContext, self.moc)
+            List {
+                ForEach(books, id: \.self) { book in
+                    NavigationLink(destination: DetailView(book: book)) {
+                        EmojiRatingView(rating: book.rating)
+                            .font(.largeTitle)
+                        
+                        VStack(alignment: .leading) {
+                            Text(book.title ?? "Unknown Title")
+                                .font(.headline)
+                            Text(book.author ?? "Unknown Author")
+                                .foregroundColor(.secondary)
+                        }
+                    }
                 }
+            }
         }
     }
 }
---
+++ Bookworm-Project11/Bookworm-Project11/EmojiRatingView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating: Int16
+
+    var body: some View {
+        switch rating {
+        case 1:
+            return Text("☹️")
+        case 2:
+            return Text("😕")
+        case 3:
+            return Text("😐")
+        case 4:
+            return Text("🙂")
+        default:
+            return Text("😀")
+        }
+    }
+}
+
+struct EmojiRatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            ForEach(1...5, id: \.self) {
+                EmojiRatingView(rating: Int16($0))
+            }
+        }
+        
+    }
+}
