214648b5c9a1110ef0435279bbed33d701ba166c
Marc Stehlin <stehlin.m@gmail.com>
2020-10-04 09:37:28 +0200

    Added Bookworm first steps, which crash at startup

---
+++ Bookworm-Project11/Bookworm-Project11/Bookworm_Project11App.swift
@@ -0,0 +1,17 @@
+//
+//  Bookworm_Project11App.swift
+//  Bookworm-Project11
+//
+//  Created by marc on 04.10.20.
+//
+
+import SwiftUI
+
+@main
+struct Bookworm_Project11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
